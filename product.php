be0dac0989a6244a2af87ba50a40c638bc512621
Jordan <berti@epse.be>
2023-04-07 15:46:40 +0200

    product page v1

--- product.php
+++ product.php
@@ -1,3 +1,14 @@
+<?php
+    require "connexion.php";
+
+    $id = htmlspecialchars($_GET['id']);
+
+    $req = $bdd->prepare("SELECT title, DATE_FORMAT(date, '%d/%m/%Y') as mydate,description,price FROM products WHERE id=?");
+    $req->execute([$id]);
+    $don=$req->fetch();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +18,10 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1> <?php echo $don['title']; ?> </h1>
+    <h1><?= $don['title'] ?></h1>
+    <h4><?= $don['mydate'] ?></h4>
+    <div><?= nl2br($don['description']) ?></div>
+    <div><?= $don['price'] ?>€</div>
 </body>
 </html>
