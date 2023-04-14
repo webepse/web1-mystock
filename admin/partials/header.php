66f0a8ba042c13aab01501283d0f503d761b81a5
Jordan <berti@epse.be>
2023-04-14 15:55:44 +0200

    admin partials deco

---
+++ admin/partials/header.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../index.php" target="_blank">Admin Stock</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="dashboard.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Products</a>
+        </li>
+      </ul>
+      <ul class="ms-auto navbar-nav">
+        <li><a href="dashboard.php?deco=ok" class="nav-link">Déconnexion</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
